218126a4c1e1ba5ca0790f91d995f69c2d5067e2
Bahaa <75643710+Beko-source@users.noreply.github.com>
2023-05-15 17:19:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from PyQt5 import  QtWidgets
+import sys
+import control
+#from control import *
+
+if __name__ == "__main__":
+     from control import Image_Classifier
+     app = QtWidgets.QApplication(sys.argv)
+     mi_app = control.MiApp()
+     mi_app.show()
+     sys.exit(app.exec_())  
+
+
+'''
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = ui_cctv_2.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+'''
