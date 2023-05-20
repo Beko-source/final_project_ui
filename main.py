4190845c40f43dc3baa261577bd62b56c5013a6b
Bahaa <75643710+Beko-source@users.noreply.github.com>
2023-05-21 00:03:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from PyQt5 import  QtWidgets
+import sys
+import control
+#from control import *
+import torch.nn as nn
+import torch
+#import video_processing 
+import os
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mi_app = control.MiApp()
+    mi_app.show()
+    sys.exit(app.exec_())
+
+
+'''
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = ui_cctv_2.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+'''
